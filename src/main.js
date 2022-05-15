b185ad1d544d22cc9c5fc9537443ee270848b65d
UniParse <103385379+TheUniParse@users.noreply.github.com>
2022-05-15 22:06:39 +0300

    optimisition

--- src/main.js
+++ src/main.js
@@ -5,41 +5,25 @@ import './party.scss'
 import mp3 from './assets/m.mp3'
 
 const div = document.querySelector('#birthday')
-div.innerHTML = '<h1>🎊Happy Birthday🎊</h1><h1>🎉Mechid🥳</h1>'
+div.innerHTML = `<h1></h1><h1>›_~</h1><audio src=${mp3} loop></audio>`
 const h1 = document.querySelectorAll('h1')
-const audio = document.createElement('audio')
-audio.setAttribute('src', mp3)
-audio.setAttribute('loop', true)
-div.appendChild(audio)
-
+const audio = document.querySelector('audio')
 const stars = document.querySelectorAll('#stars,#stars2,#stars3')
 
 
-function random(max) {
-  return Math.floor(Math.random() * (max + 1))
-}
-function randomRgb(max = 255) {
-  return `rgb(${random(max)},${random(max)},${random(max)})`
-}
-function randomHsl(saturate = 100, lightness = 50) {
-  return `hsl(${random(360)},${saturate}%,${lightness}%)`
-}
-function randomBg() {
-  document.body.style.backgroundColor = randomHsl(100, 18)
-}
-
-document.body.addEventListener('click', e => {
+document.body.addEventListener('click', e =>
   party.sparkles(e, {
     count: party.variation.range(3, 7),
     size: party.variation.range(0.8, 1.2),
-  })
+  }))
+document.addEventListener('click', () => {
+  party.confetti(h1[1], { count: party.variation.range(9, 20) })
+  h1[0].innerText = '🎊Happy Birthday🎊'
+  h1[1].innerText = '🎉Mechid🥳'
   audio.play()
-  h1[0].style.display = 'inline-block'
-  h1[1].style.display = 'inline-block'
   for (let i = 0; i < 3; i++) {
     stars[i].style.animation = `stars${i + 1} 60s 1s ease-in-out infinite alternate`
   }
-})
-document.body.addEventListener('click', e => party.confetti(h1[1], { count: party.variation.range(10, 20) }), { once: true })
+}, { once: true })
 
 //alert('done')
