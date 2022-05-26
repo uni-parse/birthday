34aa0923947b1713bb42c67164d7fb1137ac9cd5
theuniparse <theuniparse@gmail.com>
2022-05-26 17:31:31 +0100

    another big update

--- src/main.js
+++ src/main.js
@@ -33,8 +33,11 @@ function fun() {
     e.stopPropagation()
     audio[5].play()
     party.confetti(h1[1], { count: party.variation.range(9, 20) })
-    h1[0].innerText = '🎊Happy Birthday🎊'
-    h1[1].innerText = '🎉Mechid🥳'
+    h1[0].innerText = 'Happy Birthday'
+    h1[0].setAttribute('data-content', '🎊')
+    h1[1].innerText = 'Mechid'
+    h1[1].setAttribute('data-contentBefore', '🎉')
+    h1[1].setAttribute('data-contentAfter', '🥳')
     for (let i = 0; i < 3; i++) {
       stars[i].style.animation = `stars${i + 1} 60s 1s ease-in-out infinite alternate`
     }
@@ -57,17 +60,17 @@ if (typeof dialog.showModal === "function") {
         audio[3].play()
         select.disabled = true
         btn.disabled = false
-        output[0].style.color = 'hsl(90,100%,50%)'
-        output[0].innerText = 'Yes😁 2014!!\nit\'s was awesome 8 years of friendship!!'
+        output[0].style.color = 'greenyellow'
+        output[0].innerHTML = 'Yes😁 <b>2014 ✓</b>\nit\'s was awesome 8 years of <b>friendship</b>!!'
       } else if (select.value == 2014) {
         audio[1].play()
         select.disabled = true
-        output[0].style.color = 'hsl(90,100%,50%)'
-        output[0].innerText = 'Yes😁 2014!!\nit\'s was awesome 8 years of friendship!!'
+        output[0].style.color = 'greenyellow'
+        output[0].innerHTML = 'Yes😁 <b>2014 ✓</b>\nit\'s was awesome 8 years of <b>friendship</b>!!'
       } else {
         audio[2].play()
         output[0].style.color = 'darkorange'
-        output[0].innerText = `Nooo😅 we meet on 2014 not ${select.value}!!`
+        output[0].innerHTML = `Nooo😅 we meet on <span>2014 ✓</span> not <span>${select.value}✗</span> !!`
       }
     })
     date.addEventListener('change', () => {
@@ -75,17 +78,17 @@ if (typeof dialog.showModal === "function") {
         audio[3].play()
         date.disabled = true
         btn.disabled = false
-        output[1].style.color = 'hsl(90,100%,50%)'
-        output[1].innerText = 'Yes😁 15th january!!\nyou\'re younger than me by 135 day!!'
+        output[1].style.color = 'greenyellow'
+        output[1].innerHTML = 'Yes😁 <b>15<sup><small>th</small></sup> january ✓</b> !!<br>you\'re younger than me by <b>135</b> day!!'
       } else if (date.value == '2000-01-15') {
         audio[1].play()
         date.disabled = true
-        output[1].style.color = 'hsl(90,100%,50%)'
-        output[1].innerText = 'Yes😁 15th january!!\nyou\'re younger than me by 135 day!!'
+        output[1].style.color = 'greenyellow'
+        output[1].innerHTML = 'Yes😁 <b>15<sup><small>th</small></sup> january ✓</b> !!<br>you\'re younger than me by <b>135</b> day!!'
       } else {
         audio[2].play()
         output[1].style.color = 'darkorange'
-        output[1].innerText = 'Nooo😅 my birthday on 15th january!'
+        output[1].innerHTML = `Nooo😅 my birthday on <span>15<sup><small>th</small></sup> january ✓</span> not <span>${date.value}✗</span> !!`
       }
     })
     dialog.addEventListener('close', () => fun(), { once: true })
